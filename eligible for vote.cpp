b0397bf5e714e19409c0a4b149f01ce86f46c952
Monikamurugavel <92583916+Monikamurugavel@users.noreply.github.com>
2021-12-05 04:03:04 -0800

    uploaded eligible for vote.cpp
    this file is for check whether the person is eligible for vote

---
+++ eligible for vote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout << "" enter age of a user:";
+  cin >>age;
+  if(age>=18)
+  {
+    cout << "you are eligible for voting";
+  ]  
+  else{
+        cout << "you are not eligible for voting";
+      }
+ return 0;
+ }
